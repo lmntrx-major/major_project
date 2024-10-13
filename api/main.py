ff19b5c45c70cdb47086d80b498bbb82c287fbbb
lazyputrid <lazyputrid@gmail.com>
2024-10-13 23:44:32 +0530

    Got the api to work

--- api/main.py
+++ api/main.py
@@ -1,24 +1,34 @@
-from fastapi import FastAPI, UploadFile
+import uvicorn
+from fastapi import FastAPI, UploadFile, File
 import tensorflow as tf
+import os
+
+
+# Path to model location
+x: list[str] = ["model", "saved_models", "classification.keras"]
+model_rel_path: str = os.sep.join(x)
+parent_dir_path: str = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+model_path: str = os.path.join(parent_dir_path, model_rel_path)
+# print(model_path)
+
+# Loading model
+model = tf.keras.models.load_model(model_path)
+# model.summary()
 
 app = FastAPI()
 
-print(tf.__version__)
+@app.post("/predict-image")
+async def predict(file: UploadFile = File(...)):
+    image = await file.read()
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [224, 224], method=tf.image.ResizeMethod.BILINEAR, antialias=True)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image_batch = tf.expand_dims(image, 0)
 
-model = tf.keras.models.load_model("D:\\major_project_1\\models\\classification.keras")
-# async def preprocess_image(image):
-#     image = tf.image.resize(image, [224, 224], method=tf.image.ResizeMethod.BILINEAR, antialias=True)
-#     return image
-#
-# async def predict(image):
-#     return
-#
-#
-#
-#
-# @app.post("/infer/")
-# async def create_upload_file(file: UploadFile):
-#
-#
-#     return {"filename": file.filename}
+    result = tf.argmax(tf.nn.softmax(model.predict(image_batch)))
 
+    return {"Prediction": result.numpy().tolist()[0]}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
